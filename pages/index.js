01f5ded65062e90e6037d688a08ddf4717ce8964
Stephen McMurtry <smcmurtry@gmail.com>
2018-09-11 13:44:13 -0400

    added the govuk component library (#7)

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import Input from "@govuk-react/input";
 
 export class Index extends Component {
   render() {
-    return <h1>All The Steves!</h1>;
+    return (
+      <div>
+        <h1>All The Steves!</h1>
+        <Input />
+      </div>
+    );
   }
 }
 
