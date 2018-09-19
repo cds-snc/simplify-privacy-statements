e6eef9948d6eb1730f3253e8001813bdba295ac5
Stephen McMurtry <smcmurtry@gmail.com>
2018-09-19 12:26:58 -0400

    Add section headings to questionaire (#75)

--- __tests__/fixtures/questions.js
+++ __tests__/fixtures/questions.js
@@ -3,19 +3,22 @@ const questionsFixture = [
     id: "q-researcher_name",
     variable_name: "researcher_name",
     input_type: "short_text",
-    title: "Researcher Name"
+    title: "Researcher Name",
+    section_name: "section 1"
   },
   {
     id: "q-researcher_phone",
     variable_name: "researcher_phone",
     input_type: "short_text",
-    title: "Researcher Phone"
+    title: "Researcher Phone",
+    section_name: "section 1"
   },
   {
     id: "q-researcher_email",
     variable_name: "researcher_email",
     input_type: "short_text",
-    title: "Researcher Email"
+    title: "Researcher Email",
+    section_name: "section 1"
   },
   {
     id: "q-session_goal",
@@ -23,7 +26,8 @@ const questionsFixture = [
     input_type: "long_text",
     title: "What is the goal of the session?",
     hint:
-      "e.g. We are conducting research to help us evaluate the ease of use and content of a website."
+      "e.g. We are conducting research to help us evaluate the ease of use and content of a website.",
+    section_name: "section 2"
   },
   {
     id: "q-product_goal",
@@ -31,7 +35,8 @@ const questionsFixture = [
     input_type: "long_text",
     title: "What is the goal of the product?",
     hint:
-      "e.g. This will help us create an online tool that will make it easier for Veterans to determine which VAC services are relevant to them."
+      "e.g. This will help us create an online tool that will make it easier for Veterans to determine which VAC services are relevant to them.",
+    section_name: "section 2"
   },
   {
     id: "q-session_activity",
@@ -39,21 +44,24 @@ const questionsFixture = [
     input_type: "long_text",
     title: "What activity will take place during the session?",
     hint:
-      "e.g. To do this, we will ask you to complete specific tasks with this prototype and ask you to describe your thoughts and impressions."
+      "e.g. To do this, we will ask you to complete specific tasks with this prototype and ask you to describe your thoughts and impressions.",
+    section_name: "section 2"
   },
   {
     id: "q-session_duration",
     title: "How long will the session take?",
     input_type: "long_text",
     variable_name: "session_duration",
-    hint: "e.g. This will take us approximately 1 hour."
+    hint: "e.g. This will take us approximately 1 hour.",
+    section_name: "section 2"
   },
   {
     id: "q-confidentiality",
     title: "Confidentiality",
     input_type: "multiple_choice",
     variable_name: "confidentiality",
-    multiple_choice_options: ["o-confidential", "o-anonymous", "o-anonymized"]
+    multiple_choice_options: ["o-confidential", "o-anonymous", "o-anonymized"],
+    section_name: "section 3"
   }
 ];
 
--- components/questionaire.js
+++ components/questionaire.js
@@ -29,46 +29,55 @@ export class Questionaire extends Component {
     const { reduxState } = this.props;
 
     const jsx_array = [];
-    questions.forEach((q, key) => {
-      const props = {
-        className: form_group,
-        name: q.variable_name,
-        htmlFor: q.variable_name,
-        input: {
-          id: q.variable_name,
-          onChange: this.getOnChange(q.variable_name),
-          onFocus: this.getOnFocus(q.variable_name),
-          value: reduxState[q.variable_name]
-        },
-        hint: q.hint,
-        key: key
-      };
 
-      if ("multiple_choice_options" in q) {
-        q.options = reduxState.multiple_choice_options.filter(x => {
-          return q.multiple_choice_options.indexOf(x.id) > -1;
+    const section_names = Array.from(
+      new Set(questions.map(q => q.section_name))
+    );
+
+    section_names.forEach((section_name, k1) => {
+      jsx_array.push(<h3 key={k1 + "-h3"}>{section_name}</h3>);
+      questions
+        .filter(q => q.section_name === section_name)
+        .forEach((q, k2) => {
+          const props = {
+            className: form_group,
+            name: q.variable_name,
+            htmlFor: q.variable_name,
+            input: {
+              id: q.variable_name,
+              onChange: this.getOnChange(q.variable_name),
+              onFocus: this.getOnFocus(q.variable_name),
+              value: reduxState[q.variable_name]
+            },
+            hint: q.hint,
+            key: k1 + "-" + k2
+          };
+
+          if ("multiple_choice_options" in q) {
+            q.options = reduxState.multiple_choice_options.filter(x => {
+              return q.multiple_choice_options.indexOf(x.id) > -1;
+            });
+          }
+
+          if (q.input_type == "short_text") {
+            jsx_array.push(<InputField {...props}>{q.title}</InputField>);
+          } else if (q.input_type == "long_text") {
+            jsx_array.push(<TextArea {...props}>{q.title}</TextArea>);
+          } else if (q.input_type == "multiple_choice") {
+            jsx_array.push(
+              <MultipleChoice
+                hint={q.hint}
+                label={q.title}
+                name={q.variable_name}
+                options={q.options}
+                className={form_group}
+                key={k1 + "-" + k2}
+                store={this.props.store}
+              />
+            );
+          }
         });
-      }
-
-      if (q.input_type == "short_text") {
-        jsx_array.push(<InputField {...props}>{q.title}</InputField>);
-      } else if (q.input_type == "long_text") {
-        jsx_array.push(<TextArea {...props}>{q.title}</TextArea>);
-      } else if (q.input_type == "multiple_choice") {
-        jsx_array.push(
-          <MultipleChoice
-            hint={q.hint}
-            label={q.title}
-            name={q.variable_name}
-            options={q.options}
-            className={form_group}
-            key={key}
-            store={this.props.store}
-          />
-        );
-      }
     });
-
     return <React.Fragment>{jsx_array}</React.Fragment>;
   }
 }
